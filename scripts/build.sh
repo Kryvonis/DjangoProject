f9208535c11d4ba0508583b7e4d8e6d8d5fd9a8e
Kryvonis <kryvonis.artem@gmail.com>
2017-04-09 04:52:47 +0300

    fix permisions

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 sudo mkdir venv
-virtualenv -p python3 venv
+sudo virtualenv -p python3 venv
 . $(pwd)/venv/bin/activate
 pip install -r $(pwd)/requirements.txt
 cd ..
-cp /src/settings/local.sample.py /srv/www/IgoStories/igostories/settings/local.py
+sudo cp /src/settings/local.sample.py /srv/www/IgoStories/igostories/settings/local.py
 python manage.py makemigrations
 python manage.py migrate
 sh scripts/run.sh
